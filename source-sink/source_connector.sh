22b753b093250c879eb9d2929379c6e4daff5bf8
Wesley Bos <32429734+Wesley-Bos@users.noreply.github.com>
2020-08-02 16:42:05 +0200

    Change table connection

--- source-sink/source_connector.sh
+++ source-sink/source_connector.sh
@@ -5,9 +5,10 @@ curl -i -X PUT http://localhost:8083/connectors/SOURCE_POSTGRES/config \
             "connection.url":"jdbc:postgresql://postgres:5432/numbers",
             "connection.user":"pxl",
             "connection.password":"password",
+            "poll.interval.ms":"5500",
             "mode":"incrementing",
             "incrementing.column.name":"index",
             "topic.prefix":"P_",
-            "table.whitelist":"COUNTER",
+            "table.whitelist":"NUMBER",
             "validate.non.null":"false"
         }'
